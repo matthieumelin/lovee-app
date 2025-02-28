04a4e17052f47ea572e815d11c016706fec3d972
Matthieu Melin <matthieumelin62@gmail.com>
2025-02-28 03:19:32 +0100

    added tailwind

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text className='text-red-500 text-center text-3xl'>lovee</Text>
+      <View>
+        
+      </View>
+    </SafeAreaView>
+  )
+}
